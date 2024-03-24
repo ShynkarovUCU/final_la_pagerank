2ebef6fe2e0b82cad14066bfa9d7e327e4e7fe93
Yurii Shynkarov <shynkarov.pn@ucu.edu.ua>
2024-03-24 15:04:39 +0200

    SIFT visialization added

--- image_search.py
+++ image_search.py
@@ -8,17 +8,13 @@ import torch.nn as nn
 import torchvision.models as models
 import pandas as pd
 import numpy as np
-from scipy import stats
-
 import matplotlib.pyplot as plt
-import seaborn as sns
 
-import cv2
-
-import torchvision
+from cv2 import SIFT_create, BFMatcher, cvtColor, COLOR_RGB2GRAY, NORM_L2
 from torchvision import models, transforms, datasets
 
 from annoy import AnnoyIndex
+from tqdm import tqdm
 
 
 
@@ -30,6 +26,7 @@ class FoodImageSearch():
     def __init__(self) -> None:
         
         self.input_img = None
+        self.sift_similarities = None
 
 
         def _fix_names(state_dict):
@@ -137,18 +134,71 @@ class FoodImageSearch():
         features = self._get_image_features(img)
         # values = self.layer_data.values[:,1:1281].shape
 
-        self.indeces, self.distances = self.ann.get_nns_by_vector(features, n=n, include_distances=True)
+        self.ann_indeces, self.distances = self.ann.get_nns_by_vector(features, n=n, include_distances=True)
+
+        self.ann_indeces = np.array(self.ann_indeces)
+        self.distances = np.array(self.distances)
         
 
 
-    def _calculate_sift_similarity(img1, img2, similarity_ratio = 0.8):
-    
-        img1 = np.array(img1[0])
-        img2 = np.array(img2[0])
-        gray1 = cv2.cvtColor(img1, cv2.COLOR_RGB2GRAY)
-        gray2 = cv2.cvtColor(img2, cv2.COLOR_RGB2GRAY)
+    def _calculate_sift_features(self, img):
+        img = np.array(img)
+        gray = cvtColor(img, COLOR_RGB2GRAY)
 
-        sift = cv2.SIFT_create()
+        sift = SIFT_create() #cv2.SIFT_create()
+
+        kp, des = sift.detectAndCompute(gray, None)
+        
+        return des
+
+
+
+    def _calculate_sift_similarities(self, similarity_ratio = 0.8):
+        if self.input_img is None:
+            print("There is no input image registered. Use register_input() to get the result")
+        else:
+            self.sift_similarities = np.zeros(len(self.ann_indeces))
+
+            train_img_features = []
+            input_img_features = self._calculate_sift_features(self.input_img[0])
+
+            for index in tqdm(self.ann_indeces, desc='Features searching', ascii=True):
+                train_img_features.append(self._calculate_sift_features(self.train_dataset[index][0]))
+
+
+            bf = BFMatcher(normType=NORM_L2)            
+
+
+            for i, feature in tqdm(enumerate(train_img_features), desc='Similarities calculation', ascii=True):
+                if feature is None:
+                    self.sift_similarities[i] = 0
+                else:
+                    matches = bf.knnMatch(input_img_features, feature, k=2)
+                    
+                    good_matches = []
+                    for m, n in matches:
+                        if m.distance < similarity_ratio * n.distance: 
+                            good_matches.append([m])
+
+                    self.sift_similarities[i] = len(good_matches) / np.min([len(input_img_features), len(feature)])                
+                
+
+
+            print('Finalizing results...')
+            sorted_indices = np.argsort(self.sift_similarities)[::-1]  # -1 to get descending order
+            # self.sift_similarities = self.sift_similarities[sorted_indices]
+            self.sift_indeces = np.array(self.layer_data.index)[sorted_indices]
+
+
+
+    def get_sift_similarity_for_pair(self, img1, img2, similarity_ratio = 0.8):
+    
+        img1 = np.array(img1)
+        img2 = np.array(img2)
+        gray1 = cvtColor(img1, COLOR_RGB2GRAY)
+        gray2 = cvtColor(img2, COLOR_RGB2GRAY)
+
+        sift = SIFT_create() 
 
         kp1, des1 = sift.detectAndCompute(gray1, None)
         kp2, des2 = sift.detectAndCompute(gray2, None)
@@ -156,7 +206,7 @@ class FoodImageSearch():
         if des1 is None or des2 is None:   #бувало, що не знаходить дескріптори для картинки
             return 0.0 
 
-        bf = cv2.BFMatcher(normType=cv2.NORM_L2)
+        bf = BFMatcher(normType=NORM_L2)
         matches = bf.knnMatch(des1, des2, k=2)
 
         good = []
@@ -167,6 +217,11 @@ class FoodImageSearch():
         similarity_score = len(good) / np.min([len(des1), len(des2)])   #впливає дуже сильно те, як порахувати знаменник
 
         return similarity_score
+
+
+
+    def print_pretrained_model_accuracy(self):
+        print(f"Pretrained model accuracy is: {round(len(self.layer_data[self.layer_data['correct'] == self.layer_data['prediction']]) / len(self.layer_data) *100, 2)} %")
 
 
 
@@ -189,23 +244,23 @@ class FoodImageSearch():
 
 
           
-    def show_n_closest_pictures(self, n):
+    def show_n_closest_pictures(self, n, indeces):
         if self.input_img is None:
             print("There is no input image registered. Use register_input() to get the result")
-        elif n > len(self.indeces)-1:
-            print(f"N should be less or equal than {len(self.indeces)-1}")        
+        elif n > len(indeces)-1:
+            print(f"N should be less or equal than {len(indeces)-1}")        
         else:
             num_rows = (n + 4) // 5  
             plt.figure(figsize=(40, 8 * num_rows))  
 
-            for i, idx_train in enumerate(self.layer_data[self.layer_data.index.isin(self.indeces[0:n])].index):
+            for i, idx_train in enumerate(indeces[0:n]):    #enumerate(self.layer_data[self.layer_data.index.isin(indeces[0:n])].index)
                 image, label = self.train_dataset[idx_train]
 
                 # row = i // 5  
                 # col = i % 5 
                 plt.subplot(num_rows, 5, i + 1)
                 plt.imshow(image)
-                plt.title(f'Label: {self.train_dataset.classes[label]}, Distance: {round(self.distances[i], 2)}', fontsize=20)
+                plt.title(f'Label: {self.train_dataset.classes[label]}', fontsize=20) #, Distance: {round(self.distances[i], 2)}
                 plt.axis('off')
 
             plt.tight_layout() 
@@ -213,8 +268,39 @@ class FoodImageSearch():
 
 
 
-    def get_pretrained_model_accuracy(self):
-        print(f"Pretrained model accuracy is: {round(len(self.layer_data[self.layer_data['correct'] == self.layer_data['prediction']]) / len(self.layer_data) *100, 2)} %")
+    def print_accuracy_of_closest_pictures(self):
+        if self.input_img is None:
+            print("There is no input image registered. Use register_input() to get the result")
+        else:
+            closest_pics = self.layer_data[self.layer_data.index.isin(self.ann_indeces)]
+            print(f"Share of closest pictures with the same class as the input is: {round(len(closest_pics[closest_pics['correct'] == self.input_img[1]]) / len(closest_pics) * 100, 2)} %")
+
+
+
+    def print_accuracy_of_sift(self):
+        if self.input_img is None:
+            print("There is no input image registered. Use register_input() to get the result")
+        else:
+            pass
+
+
+
+    def get_sift_similarities(self):
+        if self.sift_similarities == None: 
+            self._calculate_sift_similarities()
+            return self.sift_similarities
+        else:
+            return self.sift_similarities
+
+
+
+    def get_ann_indeces_and_distances(self):
+        if self.input_img is None:
+            print("There is no input image registered. Use register_input() to get the result")
+        else:
+            return self.ann_indeces, self.distances
+
+
 
 
 
